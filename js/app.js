0eec1ad7ff8d9b7937cfb8b521f1c89e72b02602
Murshedul <murshedbinamir@gmail.com>
2022-03-11 15:10:24 +0600

    Bug-2, user profile fixed

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -133,6 +133,7 @@ const createPost = (post) => {
 };
 
 const showPosts = (posts) => {
+  console.log(posts);
     const productsContainer = document.getElementById( "posts" );
     productsContainer.innerHTML = "";
 
